97b1ea22e902eb45209ec57d20e9972dcd6b9a1e
woodzeng <635729656@qq.com>
2020-03-13 11:25:04 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,13 @@
 function test_func() {
     return 'OK';
 }
+
+/**
+ * 将当前请求的路由名称转换为CSS类名称，用于针对页面做样式定制
+ *
+ * @return void
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            版权所有
+        </p>
+        <p class="float-right">联系我们</p>
+    </div>
+</footer>
